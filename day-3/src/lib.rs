51242084883c1f4f061f054704c51e29b97c764e
carpenwd <carpenwd@gmail.com>
2024-12-26 11:41:31 -0500

    Cleaning up part 1 and part 2 and putting them into the library file

--- day-3/src/lib.rs
+++ day-3/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
 pub fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename) 
@@ -6,4 +7,48 @@ pub fn read_lines(filename: &str) -> Vec<String> {
         .lines() 
         .map(String::from) 
         .collect()
+}
+
+pub fn part_1(input_lines: &Vec<String>) -> i32 {
+        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    
+        let mut multiply_total: i32 = 0;
+        for line in input_lines {
+            let line_matches: Vec<(i32, i32)> = re.captures_iter(&line).map(|line_match| {
+                let (_, [m1, m2]) = line_match.extract();
+                (m1.parse::<i32>().unwrap(), m2.parse::<i32>().unwrap())
+            }).collect();
+    
+            for entry in line_matches {
+                multiply_total += entry.0 * entry.1;
+            }
+        }
+    
+        return multiply_total;
+}
+
+pub fn part_2(input_lines: &Vec<String>) -> i32 {
+    let re = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))").unwrap();
+
+    let mut multiply_vals = true;
+    let mut multiply_total: i32 = 0;
+    for line in input_lines {
+        let line_matches: Vec<&str> = re.find_iter(&line).map(|m| m.as_str()).collect();
+
+        for entry in line_matches {
+            if entry == "do()" {
+                multiply_vals = true;
+            } else if entry == "don't()" {
+                multiply_vals = false;
+            } else {
+                if multiply_vals {
+                    let clean_entry = entry.replace("mul(", "").replace(")", "");
+                    let digits: Vec<&str> = clean_entry.split(",").collect();
+                    multiply_total += digits[0].parse::<i32>().unwrap() * digits[1].parse::<i32>().unwrap();
+                }
+            }
+        }
+    }
+
+    return multiply_total;
 }
--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,26 +1,7 @@
-use day_3::read_lines;
-use regex::Regex;
+use day_3::{part_1, part_2, read_lines};
 
 fn main() {
     let lines = read_lines("day-3/input/input.txt");
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    
-    // For each line in the input
-    // Create a regex string that matches `mul\((\d{1,3}),(\d{1,3})\)`
-    // For each one that matches, then multiply the first group by the second
-    let mut multiply_total: i32 = 0;
-    for line in lines {
-        let line_matches: Vec<(i32, i32)> = re.captures_iter(&line).map(|line_match| {
-            let (_, [m1, m2]) = line_match.extract();
-            (m1.parse::<i32>().unwrap(), m2.parse::<i32>().unwrap())
-        }).collect();
-        //println!("{:?}", line_matches);
-
-        for entry in line_matches {
-            //println!("{:?}", entry);
-            multiply_total += entry.0 * entry.1;
-        }
-    }
-
-    println!("{}", multiply_total);
+    println!("Part-1 Total: {}", part_1(&lines));
+    println!("Part-2 Total: {}", part_2(&lines));
 }
